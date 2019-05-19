4c248e70ff87538a71d0caf89d12c36d3437a334
Tim <tim@apples-mbp.mynet>
2019-05-19 08:30:56 +0800

    Initial Commit

---
+++ SummerDrinks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SummerDrinks
+//
+//  Created by Tim on 19/5/2019.
+//  Copyright © 2019 Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
